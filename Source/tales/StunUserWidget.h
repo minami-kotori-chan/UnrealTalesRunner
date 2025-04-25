48fb151b054600678841caaa46de8479b13e8981
kotori <kotori@minami-kotori.com>
2025-04-26 01:13:36 +0900

    스턴UI구현

---
+++ Source/tales/StunUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StunUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TALES_API UStunUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
